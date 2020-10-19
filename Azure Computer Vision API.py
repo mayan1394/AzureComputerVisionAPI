8177c6fb07bcd29328fde664ec6be6684decab0e
Mayank <accessmayankjain@gmail.com>
2020-10-19 15:45:35 +0530

    Update Azure Computer Vision API.py

--- Azure Computer Vision API.py
+++ Azure Computer Vision API.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
-pip freeze
-
-
-# In[1]:
-
-
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
 from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
@@ -21,38 +12,13 @@ import sys
 import time
 import pandas as pd
 
-
-# In[2]:
-
-
-# Add your Computer Vision subscription key to your environment variables.
-if 'COMPUTER_VISION_SUBSCRIPTION_KEY' in os.environ:
-    subscription_key = os.environ['COMPUTER_VISION_SUBSCRIPTION_KEY']
-else:
-    print("\nSet the COMPUTER_VISION_SUBSCRIPTION_KEY environment variable.\n**Restart your shell or IDE for changes to take effect.**")
-    sys.exit()
-# Add your Computer Vision endpoint to your environment variables.
-if 'COMPUTER_VISION_ENDPOINT' in os.environ:
-    endpoint = os.environ['COMPUTER_VISION_ENDPOINT']
-else:
-    print("\nSet the COMPUTER_VISION_ENDPOINT environment variable.\n**Restart your shell or IDE for changes to take effect.**")
-    sys.exit()
-
-
-# In[3]:
-
-
 computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
 
-
 # In[4]:
-
-
 os.chdir("C:\Project Work\Mayank_Data\GCP Test")
 
 
 # In[5]:
-
 
 def getimagedescription(image):
     image = open(image,"rb")
